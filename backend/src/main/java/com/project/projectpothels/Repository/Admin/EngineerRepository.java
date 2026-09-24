b22718fd8e93d8dda2c0e89d2389ed10bd8e2e2d
DEEPAK07-KD <ffdeepak0007@gmail.com>
2026-09-24 20:18:22 +0530

    Fix incorrect imports in EngineerRepository

--- backend/src/main/java/com/project/projectpothels/Repository/Admin/EngineerRepository.java
+++ backend/src/main/java/com/project/projectpothels/Repository/Admin/EngineerRepository.java
@@ -1,12 +1,10 @@
 package com.project.projectpothels.Repository.Admin;
 
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.projectpothels.model.Admin.EngineerModel;
 
-import java.lang.classfile.ClassFile.Option;
 import java.util.List;
 import java.util.Optional;
 
